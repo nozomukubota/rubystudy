67bcf78e4e31f14bbb090252f45e7528f8c49710
Nozomu Kubota <nozomu.k@cnxt.jp>
2015-06-28 13:15:16 +0900

    181Pまで読了

---
+++ itemnil.rb
@@ -0,0 +1,14 @@
+def ary
+end
+=begin
+
+item=nil
+if ary
+	item=ary[0]
+end
+p item
+		
+=end
+
+item=ary&&ary[0]
+p item
---
+++ range.rb
@@ -0,0 +1,6 @@
+p Range.new(1,10)
+p Range.new(1,10).to_a
+p (5..10).to_a
+p (5...10).to_a
+p ("a".."f").to_a
+p ("a"..."f").to_a
